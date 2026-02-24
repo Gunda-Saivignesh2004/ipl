80b30d52412e1956fad858699999231f826f5d92
Gunda-Saivignesh2004 <dummy@gmail.com>
2026-02-24 16:31:30 +0000

    day4

--- server/src/main/java/com/edutech/progressive/entity/Match.java
+++ server/src/main/java/com/edutech/progressive/entity/Match.java
@@ -1,8 +1,16 @@
 package com.edutech.progressive.entity;
  
 import java.util.Date;
- 
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+ @Entity
 public class Match {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int matchId;
     private int firstTeamId;
     private int secondTeamId;
--- server/src/main/java/com/edutech/progressive/entity/Team.java
+++ server/src/main/java/com/edutech/progressive/entity/Team.java
@@ -1,6 +1,15 @@
 package com.edutech.progressive.entity;
- 
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int teamId;
     private String teamName;
     private String location;
